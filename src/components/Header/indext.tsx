8525a233db6b3c9c663eaa38141dc2daf423a27c
Rafael de Luca <deluca1712@gmail.com>
2023-08-25 14:21:56 -0300

    layout from initial home page

--- src/components/Header/indext.tsx
+++ src/components/Header/indext.tsx
@@ -3,9 +3,9 @@ import './styles.css';
 const Header = () => {
     return (
         <>
-            <header>
-                <div>
-                    <h1>Site Home DeLuca</h1>
+            <header className="header-container">
+                <div className="container">
+                    <h1 className="site-title">Site Home DeLuca</h1>
                 </div>
             </header>
         </>
--- src/pages/Home/HomeBody/index.tsx
+++ src/pages/Home/HomeBody/index.tsx
@@ -1,10 +1,23 @@
+import Button from '../../../components/Button';
+import Card from '../../../components/Card/indext';
 import './styles.css';
 
 const HomeBody = () => {
     return (
         <>
             <main>
-                <h2>Body of initial home page</h2>
+                <section>
+                    <div className="container">
+                        <div className="margin-top margin-botton">
+                            <Card title="Body of initial home page"></Card>
+                            <div className="display-flex">
+                                <Button message={'See Promotions'}></Button>
+                            </div>
+                        </div>
+                    </div>
+
+                </section>
+
             </main>
         </>
     );
